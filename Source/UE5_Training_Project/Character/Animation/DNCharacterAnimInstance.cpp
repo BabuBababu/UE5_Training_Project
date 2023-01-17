4058253ea15589e0345206cd0594aa03dc107af8
BabuBababu <poryou77@gmail.com>
2023-01-17 15:52:49 +0900

    [AnimInstance] 애님 인스턴스 구현

---
+++ Source/UE5_Training_Project/Character/Animation/DNCharacterAnimInstance.cpp
@@ -0,0 +1,73 @@
+﻿
+#include "UE5_Training_Project/Character/Animation/DNCharacterAnimInstance.h"
+
+// Engine
+#include <GameFramework/PawnMovementComponent.h>
+#include <Kismet/KismetMathLibrary.h>
+
+// Character
+#include "UE5_Training_Project/Character/DNCommonCharacter.h"
+
+
+
+
+UDNCharacterAnimInstance::UDNCharacterAnimInstance()
+{
+}
+
+void UDNCharacterAnimInstance::NativeBeginPlay()
+{
+}
+
+void UDNCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (IsValid(TryGetPawnOwner()) == false)
+		return;
+
+	calculate_speed_direction(TryGetPawnOwner());
+
+	_bIsInAir = TryGetPawnOwner()->GetMovementComponent()->IsFalling();
+
+	ADNCommonCharacter* character = dynamic_cast<ADNCommonCharacter*>(TryGetPawnOwner());
+
+	if (character == nullptr)
+		return;
+
+
+	_character_state = character->_character_state ;
+	_aiming = character->_is_aiming;
+	_near_wall = character->_is_near_wall;
+	_sprinting = character->_is_sprint;
+	_crouching = character->_is_crouch;
+	_firing = character->_is_fire;
+	_climbing = character->GetMovementComponent()->IsFlying();
+
+
+
+
+	// 조준할 때 상체 위아래 블렌딩
+	if (_aiming)
+	{
+		float angle = UKismetMathLibrary::ClampAngle(character->GetBaseAimRotation().Pitch, -55.f, 55.f);
+		FRotator rotator = UKismetMathLibrary::MakeRotator(angle, 0.f, 0.f);
+
+		_aiming_for_spine = rotator.GetInverse();
+
+	}
+	else
+		_aiming_for_spine = FRotator(0.f, 0.f, 0.f);
+}
+
+
+
+void UDNCharacterAnimInstance::calculate_speed_direction(APawn* pawn_in)
+{
+	_speed = pawn_in->GetVelocity().Length();
+
+	_direction = CalculateDirection(pawn_in->GetVelocity(), pawn_in->GetActorRotation());
+	
+
+
+}
---
+++ Source/UE5_Training_Project/Character/Animation/DNCharacterAnimInstance.h
@@ -0,0 +1,87 @@
+﻿
+
+#pragma once
+
+
+// Engine
+#include <CoreMinimal.h>
+#include <Animation/AnimInstance.h>
+
+// Defs
+#include "UE5_Training_Project/Defs/DNDefs.h"
+
+// generated
+#include "DNCharacterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_TRAINING_PROJECT_API UDNCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UDNCharacterAnimInstance();
+
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void calculate_speed_direction(APawn* pawn_in);
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	float _speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	float _direction;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	FRotator _aiming_for_spine;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	bool _bIsInAir;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	bool _sprinting;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	bool _aiming;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	bool _climbing;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	bool _crouching;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	bool _IK_Enabled;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	E_CHARACTER_STATE _character_state;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	bool _near_wall;
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	float _ADS_BlendSpeed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	bool _is_FPP;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	float _mouse_x;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	FTransform _LeftHandIK_Socket;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+	bool _firing;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = MyInstance, meta = (AllowPrivateAccess = "true"))
+		FVector _actor_location;
+};
