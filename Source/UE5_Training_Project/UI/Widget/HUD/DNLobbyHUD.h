d1032513b501f197499172a256eeabea1534271b
BabuBababu <poryou77@gmail.com>
2023-03-20 14:15:34 +0900

    로비 레벨 추가, 로비용 HUD, 플레이어 캐릭터 추가 및 구현중

---
+++ Source/UE5_Training_Project/UI/Widget/HUD/DNLobbyHUD.h
@@ -0,0 +1,22 @@
+﻿
+
+#pragma once
+
+// Engine
+#include <CoreMinimal.h>
+
+// Base
+#include "UE5_Training_Project/UI/Base/DNBaseHUD.h"
+
+// Generated
+#include "DNLobbyHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_TRAINING_PROJECT_API ADNLobbyHUD : public ADNBaseHUD
+{
+	GENERATED_BODY()
+	
+};
